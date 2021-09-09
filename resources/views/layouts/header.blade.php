5f9ea43b295ad9a9a8331801df3d651a441dc98c
Programmer Dog and Rooster <programmer@dogandrooster.com>
2021-09-09 19:47:01 +0800

    7%

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,28 @@
+
+<div class="header">
+    <div class="row">
+        <div class="banner column">
+            <a class="banner-logo active">
+                <img class="logo-image" src="{{asset('images/oa-logo.png')}}">
+            </a>
+        </div>
+        <div class="column">
+            <div class="banner-item">
+                <ul class="navbar">
+                    <li class="nav-item">About OA</li>
+                    <li class="nav-item">About SP</li>
+                    <li class="nav-item active">Login</li>
+                </ul>
+               
+            </div>
+            <div class="banner-item-icon">
+                <a href="javascript:void(0);" class="header-icon">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+           
+        </div>
+       
+    </div>
+   
+</div>
